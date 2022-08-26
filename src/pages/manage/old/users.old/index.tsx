735b3e4fa44bddfbc0ca4426767a4c663b1e4241
Danilo Santana <101213820+danilosantanadeveloper@users.noreply.github.com>
2022-08-26 09:15:40 -0300

    pages

---
+++ src/pages/manage/old/users.old/index.tsx
@@ -0,0 +1,28 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+
+const Users = () => {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Usuários'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>Usuários.</Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              ornare metus vitae convallis faucibus. Sed at lorem justo. Vivamus
+              eu nisi id eros tincidunt tincidunt id nec mi. Aliquam non finibus.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Users
