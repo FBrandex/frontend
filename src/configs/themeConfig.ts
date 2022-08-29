8401bd76a43f7a4990fae61dc5cb9a1bdeebfa73
Danilo Santana <101213820+danilosantanadeveloper@users.noreply.github.com>
2022-08-29 10:48:53 -0300

    Update themeConfig.ts

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -44,7 +44,7 @@ type ThemeConfig = {
 
 const themeConfig: ThemeConfig = {
   // ** Layout Configs
-  templateName: 'X Prop' /* App Name */,
+  templateName: 'XPROP' /* App Name */,
   layout: 'vertical' /* vertical | horizontal */,
   mode: 'dark' /* light | dark */,
   direction: 'ltr' /* ltr | rtl */,
