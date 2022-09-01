89bade2566d811d97c4428daec133a55ba83777d
Danilo Santana <101213820+danilosantanadeveloper@users.noreply.github.com>
2022-09-01 09:28:18 -0300

    minimal files

--- src/@fake-db/index.ts
+++ src/@fake-db/index.ts
@@ -1,13 +1,6 @@
 import mock from './mock'
 
-import './cards'
 import './auth/jwt'
-import './pages/faq'
-import './autocomplete'
 import './manage/userList'
-import './app-bar-search'
-import './pages/knowledge-base'
-import './server-side-menu/vertical'
-import './server-side-menu/horizontal'
 
 mock.onAny().passThrough()
