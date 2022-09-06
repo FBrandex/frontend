e0b82595afe42d1e70643cab772dbfdb46de2c73
Danilo Santana <101213820+danilosantanadeveloper@users.noreply.github.com>
2022-09-06 11:31:18 -0300

    Update backdrop.ts

--- src/@core/theme/overrides/backdrop.ts
+++ src/@core/theme/overrides/backdrop.ts
@@ -4,16 +4,13 @@ import { Theme } from '@mui/material/styles'
 // ** Util Import
 import { hexToRGBA } from 'src/@core/utils/hex-to-rgba'
 
-// ** Drawer background (modal alpha)
 const Backdrop = (theme: Theme) => {
   return {
     MuiBackdrop: {
       styleOverrides: {
         root: {
           backgroundColor:
-            theme.palette.mode === 'light'
-              ? `rgba(${theme.palette.customColors.main}, 0.5)`
-              : hexToRGBA('#000000', 0.45)
+            `rgba(${theme.palette.customColors.darkBg}, 0.5)`
         },
         invisible: {
           backgroundColor: 'transparent'
