064312876be8bedb0a0dddcbc76c35017051d516
Danilo Santana <101213820+danilosantanadeveloper@users.noreply.github.com>
2022-10-17 15:20:49 -0300

    new page smtp

---
+++ src/pages/setting/smtp/index.tsx
@@ -0,0 +1,25 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+
+const SmtpConfig = () => {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Configuração SMTP'></CardHeader>
+          <CardContent>
+            <Typography>
+              Configurar email do remetente, servidor de saída, porta SMTP.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default SmtpConfig
