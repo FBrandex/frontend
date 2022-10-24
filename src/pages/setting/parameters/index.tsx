bd5f8aba200660ee6c358967d343611912454f5b
Danilo Santana <101213820+danilosantanadeveloper@users.noreply.github.com>
2022-10-24 10:29:11 -0300

    parameters page

---
+++ src/pages/setting/parameters/index.tsx
@@ -0,0 +1,25 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+
+const ParametersConfig = () => {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Configuração SMTP'></CardHeader>
+          <CardContent>
+            <Typography>
+              Configurar parametrização, para flexibilização de API.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default ParametersConfig
